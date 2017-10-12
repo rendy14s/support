a8e0f3583d804f302be269565171fe93b88edb9a
wira <swira110@gmail.com>
2017-10-13 03:09:19 +0700

    add modul detail

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, ViewController  } from 'ionic-angular';
 
 /**
  * Generated class for the DetailPage page.
@@ -15,11 +15,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public viewCtrl: ViewController ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DetailPage');
+  }
+  dismiss() {
+    this.viewCtrl.dismiss();
   }
 
 }
