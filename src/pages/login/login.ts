cfcc122d34d00a267337d6e4e5b7751b6234a7c5
Acis <akislatuconsina@gmail.com>
2017-10-13 20:03:53 +0700

    New Upload

---
+++ src/pages/login/login.ts
@@ -0,0 +1,35 @@
+import { HomePage } from './../home/home';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams,  ModalController } from 'ionic-angular';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public modalCtrl: ModalController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  presentModal() {
+    let modal = this.modalCtrl.create("RegistrationPage");
+    modal.present();
+  }
+  login() {
+    this.navCtrl.setRoot(HomePage)
+  }
+}
