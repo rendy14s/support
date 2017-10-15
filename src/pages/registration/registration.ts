813845bf22704757057cfe53af273789d76a5411
Ren <rendy.sutandy@dataon.com>
2017-10-16 01:22:44 +0700

    remove some function not needed

--- src/pages/registration/registration.ts
+++ src/pages/registration/registration.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController,  ModalController, ViewController} from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ViewController} from 'ionic-angular';
 
 @IonicPage()
 @Component({
