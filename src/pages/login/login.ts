c9e54a8aaf8714198ff299d9e4faefe9e34d2ed7
Ren <rendy.sutandy@dataon.com>
2017-10-27 00:58:16 +0700

    update sdk

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { Component, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
-import { UserApi } from './../../shared/sdk/services/custom/user';
+import { VMDUsercredentialApi } from './../../shared/sdk/services/custom/VMDUsercredential';
 
 /**
  * Generated class for the LoginPage page.
@@ -14,55 +14,55 @@ import { UserApi } from './../../shared/sdk/services/custom/user';
   selector: 'page-login',
   templateUrl: 'login.html',
   animations: [
-  
-     //For the logo
-     trigger('flyInBottomSlow', [
-       state('in', style({
-         transform: 'translate3d(0,0,0)'
-       })),
-       transition('void => *', [
-         style({transform: 'translate3d(0,2000px,0'}),
-         animate('2000ms ease-in-out')
-       ])
-     ]),
-  
-     //For the background detail
-     trigger('flyInBottomFast', [
-       state('in', style({
-         transform: 'translate3d(0,0,0)'
-       })),
-       transition('void => *', [
-         style({transform: 'translate3d(0,2000px,0)'}),
-         animate('1000ms ease-in-out')
-       ])
-     ]),
-  
-     //For the login form
-     trigger('bounceInBottom', [
-       state('in', style({
-         transform: 'translate3d(0,0,0)'
-       })),
-       transition('void => *', [
-         animate('2000ms 200ms ease-in', keyframes([
-           style({transform: 'translate3d(0,2000px,0)', offset: 0}),
-           style({transform: 'translate3d(0,-20px,0)', offset: 0.9}),
-           style({transform: 'translate3d(0,0,0)', offset: 1})
-         ]))
-       ])
-     ]),
-  
-     //For login button
-     trigger('fadeIn', [
-       state('in', style({
-         opacity: 1
-       })),
-       transition('void => *', [
-         style({opacity: 0}),
-         animate('1000ms 2000ms ease-in')
-       ])
-     ])
-   ]
- })
+
+    //For the logo
+    trigger('flyInBottomSlow', [
+      state('in', style({
+        transform: 'translate3d(0,0,0)'
+      })),
+      transition('void => *', [
+        style({ transform: 'translate3d(0,2000px,0' }),
+        animate('2000ms ease-in-out')
+      ])
+    ]),
+
+    //For the background detail
+    trigger('flyInBottomFast', [
+      state('in', style({
+        transform: 'translate3d(0,0,0)'
+      })),
+      transition('void => *', [
+        style({ transform: 'translate3d(0,2000px,0)' }),
+        animate('1000ms ease-in-out')
+      ])
+    ]),
+
+    //For the login form
+    trigger('bounceInBottom', [
+      state('in', style({
+        transform: 'translate3d(0,0,0)'
+      })),
+      transition('void => *', [
+        animate('2000ms 200ms ease-in', keyframes([
+          style({ transform: 'translate3d(0,2000px,0)', offset: 0 }),
+          style({ transform: 'translate3d(0,-20px,0)', offset: 0.9 }),
+          style({ transform: 'translate3d(0,0,0)', offset: 1 })
+        ]))
+      ])
+    ]),
+
+    //For login button
+    trigger('fadeIn', [
+      state('in', style({
+        opacity: 1
+      })),
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('1000ms 2000ms ease-in')
+      ])
+    ])
+  ]
+})
 
 export class LoginPage {
   public logoState: any = "in";
@@ -71,11 +71,11 @@ export class LoginPage {
   public formState: any = "in";
   public username: any;
   public password: any;
- 
-  constructor(public navCtrl: NavController, 
+
+  constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public modalCtrl: ModalController,
-    public userApi: UserApi
+    public VMDusercredentialApi: VMDUsercredentialApi
   ) {
   }
 
@@ -87,17 +87,18 @@ export class LoginPage {
     let modal = this.modalCtrl.create("RegistrationPage");
     modal.present();
   }
-  login() {
-    this.userApi.find({
+
+  public login() {
+    this.VMDusercredentialApi.find({
       where: {
         and: [
           { username: this.username },
           { password: this.password }
         ]
       }
-    }).subscribe((loginDo) => {
-      console.log(loginDo);
+    }).subscribe((result) => {
+      console.log(result);
     })
-    // this.navCtrl.setRoot('HomePage', { name: this.username }); //fungsi ini untuk melempar variable
   }
+
 }
