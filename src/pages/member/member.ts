588ccc9c602121e82f0528550f3b521d12f23943
atsun <adjietirta13@gmail.com>
2017-11-09 21:43:21 +0700

    maybe i make a mistaken.. but just do it

--- src/pages/member/member.ts
+++ src/pages/member/member.ts
@@ -3,6 +3,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { VMDMasterEmmployeeCredentialApi } from './../../shared/sdk/services/custom/VMDMasterEmmployeeCredential';
 import { VMDMasteruserdataApi } from './../../shared/sdk/services/custom/VMDMasteruserdata';
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 /**
  * Generated class for the MemberPage page.
  *
@@ -17,6 +21,7 @@ import { VMDMasteruserdataApi } from './../../shared/sdk/services/custom/VMDMast
 })
 
 export class MemberPage {
+  public viewmember:  any;
   public member: any;
   public member2: any;
   public listmember: any;
@@ -24,13 +29,13 @@ export class MemberPage {
   public memberfullname: any;
   public memberemail: any;
   public id: any;
-  public viewmember: any;
 
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public VMDMasteruserdata: VMDMasteruserdataApi,
     public storage: Storage,
+    public DataUser: VMDMasteruserdataApi
   ) {
   }
 
@@ -41,7 +46,7 @@ export class MemberPage {
       this.member2 = this.member.user.id
       console.log(this.member2);
       // this.VMDUserMember.find
-      this.VMDMasteruserdata.find({
+      this.DataUser.find({
         where: {
           id: this.id
         }
